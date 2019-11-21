1eac47029b8ec3749bfda117cc9474aa52f93ff8
Lucas Mello <lucashsmello@gmail.com>
2019-11-21 09:33:12 -0300

    Pair plot visualization of embedding network

--- visualization.py
+++ visualization.py
@@ -1,10 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+import pandas as pd
+
 
 _COLORS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
            '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
            '#bcbd22', '#17becf']
+
+
+def pairplot_embeddings(embeddings, targets, classes):
+    data = {'f%d' % (i+1): embeddings[:, i] for i in range(embeddings.shape[1])}
+    df = pd.DataFrame(data)
+    df['class'] = [classes[t][0] for t in targets]
+    mypalette = {classes[c][0]: classes[c][1] for c in classes}
+    sns.pairplot(df, hue="class", kind='scatter', palette=mypalette, plot_kws={'alpha': 0.5})
+    plt.show()
 
 
 def plot_embeddings(embeddings, targets, classes, xlim=None, ylim=None):
